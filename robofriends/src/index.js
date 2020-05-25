298cbe651d5caf2e44edf399e3baa920e91dbd05
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2020-05-25 13:57:33 +0530

    code cleanup

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./scss/main.scss";
 import * as serviceWorker from "./serviceWorker";
-import App from "./components/App";
+import App from "./containers/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
