34799ea768ddd64c325ba3acd129e6dfca52c2c0
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2020-05-25 12:01:54 +0530

    search robo setup

---
+++ robofriends/src/components/SearchBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBox = (props) => {
+  return (
+    <div className="w-50 mx-auto mb-2">
+      <input type="text" className="form-control" placeholder="Search Robos" />
+    </div>
+  );
+};
+
+export default SearchBox;
